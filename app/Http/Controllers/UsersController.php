87c39f721b6d8805259775ef50ab05c68818ca96
seiya <skkasans@gmail.com>
2020-08-23 12:26:58 +0900

    画像をamazon S3に保存するように修正

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -97,7 +97,7 @@ class UsersController extends Controller
         {
             $disk = Storage::disk('s3');
             $fileName = $disk->put('', $request->file('image_path'));
-            $user->image_path = $disk->url($fileName);
+            $user->image_path = $fileName;
             $user->name = $request->name;
             $user->content = $request->content;
 
